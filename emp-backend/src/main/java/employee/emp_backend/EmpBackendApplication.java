f786f758d3cd317ad7c4f39a02e9f12fcfa8d632
Yogirajkhalate <yogiraj9696khalate@gmail.com>
2025-03-08 00:33:54 +0530

    backend

---
+++ emp-backend/src/main/java/employee/emp_backend/EmpBackendApplication.java
@@ -0,0 +1,13 @@
+package employee.emp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpBackendApplication.class, args);
+	}
+
+}
---
+++ emp-backend/src/test/java/employee/emp_backend/EmpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package employee.emp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
