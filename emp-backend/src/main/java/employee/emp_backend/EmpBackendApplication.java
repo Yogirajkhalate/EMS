eada0136c65f4f640808138063e30a9ed97b564b
Yogirajkhalate <yogiraj9696khalate@gmail.com>
2025-03-08 12:59:57 +0530

    add Entity

--- emp-backend/src/main/java/employee/emp_backend/EmpBackendApplication.java
+++ emp-backend/src/main/java/employee/emp_backend/EmpBackendApplication.java
@@ -2,15 +2,14 @@ package employee.emp_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("entity")
 public class EmpBackendApplication {
-
 	public static void main(String[] args) {
-		
 		System.out.println("Check");
-		SpringApplication.run(EmpBackendApplication.class, args);
-		
+		SpringApplication.run(EmpBackendApplication.class, args);	
 	}
 
 }
---
+++ emp-backend/src/main/java/entity/Employee.java
@@ -0,0 +1,65 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="employees")
+
+public class Employee {
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // for auto increment
+	private Long id;
+	
+	@Column(name ="first_name") //for column name
+	private String firstName;
+	
+	@Column(name ="last_name")
+	private String lastName;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Column(name ="email" , nullable = false)
+	private String email;
+	
+
+}
